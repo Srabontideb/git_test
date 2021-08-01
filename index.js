b6233ecb7b7f827fc9b1a9798c48ff5676169db8
Srabontideb <69215107+Srabontideb@users.noreply.github.com>
2021-08-01 15:07:13 +0600

    Introduction to basic javascript

---
+++ index.js
@@ -0,0 +1,39 @@
+// variable best practise
+var number = 20
+var number1 = 30.5
+
+console.log(number+number1)
+console.log(typeof(number1))
+
+
+// parseFloat dile fraction part o nibe
+var number2 = parseInt("50.56")
+var number3 = 60
+
+console.log(typeof(number2))
+console.log(number2+number3)+"br"
+
+var age = "Hello world"
+console.log(age.indexOf("world"))
+
+var ishot = true
+console.log(typeof(ishot))
+
+// number to string convert
+var numb = 15
+var n2s = numb.toString()
+console.log(typeof(n2s))
+
+
+var num1 = 0.2
+var num2 = 0.4
+total = num1+num2
+console.log(total.toFixed(3))
+
+
+var a = -5
+console.log(Math.abs(a))
+
+var b = Math.random()*1000
+var changeB = Math.round(b)
+console.log(changeB)
